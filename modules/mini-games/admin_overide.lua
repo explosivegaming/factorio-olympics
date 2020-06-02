44b70f18a1846821731987515b7f91b3b0eeba08
tovernaar123 <ben.schellevis@outlook.com>
2020-06-02 15:08:28 +0200

    urgh

--- modules/mini-games/admin_overide.lua
+++ modules/mini-games/admin_overide.lua
@@ -12,7 +12,7 @@ Commands.new_command('start','Command to start a mini_game.')
 :add_param('option2',true)
 :add_param('option3',true)
 :add_param('option4',true)
-:register(function(_,name_of_game,option1,option2,option3,option4,raw)
+:register(function(_,name_of_game,option1,option2,option3,option4,_)
     local succes = Mini_games.start_game(name_of_game,{option1,option2,option3,option4})
     if succes  then
         return  Commands.error(succes)
