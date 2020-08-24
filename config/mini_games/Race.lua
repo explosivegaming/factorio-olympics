6b481c77a28b13a3916f26bed458ef87a9ca6ac6
tovernaar123 <ben.schellevis@outlook.com>
2020-08-24 15:39:25 +0200

    added map2

---
+++ config/mini_games/Race.lua
@@ -0,0 +1,32 @@
+return {
+    {
+        name = 'Cronoks',
+        surface_name = 'Race game2';
+        start_pos = {
+            left = {
+                x = 288,
+                y = -160
+            },
+            right = {
+                x = 300,
+                y = -157
+            }
+        },
+        cheater_orentation = 0.5
+    },
+    {
+        name = 'samy115',
+        surface_name = 'Race game';
+        start_pos = {
+            left = {
+                x = -85,
+                y = -126
+            },
+            right = {
+                x = -75,
+                y = -126
+            }
+        },
+        cheater_orentation = 0.25
+    }
+}
